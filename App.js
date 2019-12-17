a3d17f30d9a26d0f0aeca1e7c37873d5f8a5242c
sid.shi <sid.shi@nirvana-info.com>
2019-12-17 13:15:00 +0800

    init

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import { View } from "react-native";
-import MySelect from "./MySelect";
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <MySelect />
+        <Text> hello </Text>
       </View>
     );
   }
